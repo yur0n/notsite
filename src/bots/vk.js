2cc0e6006e35152d04f04a4a1e503500f81dc663
Yuri <yur0n@yahoo.com>
2021-07-25 20:30:40 +0300

    VK bot optimization

--- src/bots/vk.js
+++ src/bots/vk.js
@@ -4,75 +4,66 @@ const bot = new Slimbot('1928057359:AAEe94RZJ7oheBjlyE-gThde79JhU72mSJU')
 
 
 // const yur0n = 378931386
-const rudi = 267424833
+const rudi = 267424833   //chat id of rudi and this bot
+const line = '➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖'
+const inlineKeyboard = (url1, url2) => {        // inline keyboard as object of parameters for bot.sendMessage command
+    return {                                    // url1 = link to от кого, url2 = link to подробнее
+        parse_mode: 'Markdown',                 
+        reply_markup: JSON.stringify({
+            inline_keyboard: [[{ text: "От кого?", url: url1 },
+            { text: "Подробнее", url: url2 }]]
+        })
+    }
+}  
 
 bot.on('message',  mes => {
     bot.sendMessage(mes.chat.id, 'Я буду автоматически присылать новую информацию о твоей группе.')
-    console.log(mes.chat.id)
+    console.log(mes.chat.id, mes.text)
 })
 
 const getData = async (data) => {
-try {    
-    const line = '➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖'
-    const optionsMarketComment = {
-        parse_mode: 'Markdown',
-        reply_markup: JSON.stringify({
-          inline_keyboard: [[{ text: "От кого?", url: `https://vk.com/id${data.object.from_id}`}, 
-          { text: "Подробнее", url: `https://vk.com/market-124949590?w=product-124949590_${data.object.item_id}`}]]})}
+try {         
     switch (data.type) {
         case 'wall_reply_new':
-            let options1 = {
-                parse_mode: 'Markdown',
-                reply_markup: JSON.stringify({
-                  inline_keyboard: [[{ text: "От кого?", url: `https://vk.com/id${data.object.from_id}`}, 
-                  { text: "Подробнее", url: `https://vk.com/sib_herb?w=wall-124949590_${data.object.post_id}`}]]})}
             await bot.sendMessage(rudi, 'Добавление комментария на стене:')
-            await bot.sendMessage(rudi, data.object.text, options1)
+            await bot.sendMessage(rudi, data.object.text, inlineKeyboard
+                (`https://vk.com/id${data.object.from_id}`, `https://vk.com/sib_herb?w=wall-124949590_${data.object.post_id}`))
                   bot.sendMessage(rudi, line)
             break        
         case 'message_new':
-            let options0 = {
-                parse_mode: 'Markdown',
-                reply_markup: JSON.stringify({
-                  inline_keyboard: [[{ text: "От кого?", url: `https://vk.com/id${data.object.message.from_id}`}, 
-                  { text: "Подробнее", url: `https://vk.com/im?sel=-${data.group_id}`}]]})}
             await bot.sendMessage(rudi, 'Входящее сообщение:')
-            await bot.sendMessage(rudi, data.object.message.text, options0)
+            await bot.sendMessage(rudi, data.object.message.text, inlineKeyboard
+                (`https://vk.com/id${data.object.message.from_id}`, `https://vk.com/im?sel=-${data.group_id}`))
                   bot.sendMessage(rudi, line)
             break        
         case 'board_post_new':
-            let options2 = {
-                parse_mode: 'Markdown',
-                reply_markup: JSON.stringify({
-                  inline_keyboard: [[{ text: "От кого?", url: `https://vk.com/id${data.object.from_id}`}, 
-                  { text: "Подробнее", url: `https://vk.com/topic-124949590_${data.object.topic_id}`}]]})}
             await bot.sendMessage(rudi, 'Создание комментария в обсуждении:')
-            await bot.sendMessage(rudi, data.object.text, options2)
+            await bot.sendMessage(rudi, data.object.text, inlineKeyboard
+                (`https://vk.com/id${data.object.from_id}`, `https://vk.com/topic-124949590_${data.object.topic_id}`))
                   bot.sendMessage(rudi, line)     
             break
         case 'market_comment_new':
             await bot.sendMessage(rudi, 'Новый комментарий к товару:')
-            await bot.sendMessage(rudi, data.object.text, optionsMarketComment)
+            await bot.sendMessage(rudi, data.object.text, inlineKeyboard
+                (`https://vk.com/id${data.object.from_id}`, `https://vk.com/market-124949590?w=product-124949590_${data.object.item_id}`))
                   bot.sendMessage(rudi, line)
             break
         case 'market_comment_edit':
             await bot.sendMessage(rudi, 'Редактирование комментария к товару:')
-            await bot.sendMessage(rudi, data.object.text, optionsMarketComment)
+            await bot.sendMessage(rudi, data.object.text, inlineKeyboard
+                (`https://vk.com/id${data.object.from_id}`, `https://vk.com/market-124949590?w=product-124949590_${data.object.item_id}`))
                   bot.sendMessage(rudi, line)  
             break
         case 'market_comment_restore':
             await bot.sendMessage(rudi, 'Восстановление комментария к товару:')
-            await bot.sendMessage(rudi, data.object.text, optionsMarketComment)
+            await bot.sendMessage(rudi, data.object.text, inlineKeyboard
+                (`https://vk.com/id${data.object.from_id}`, `https://vk.com/market-124949590?w=product-124949590_${data.object.item_id}`))
                   bot.sendMessage(rudi, line)  
             break
         case 'vkpay_transaction':
-            let options3 = {
-                parse_mode: 'Markdown',
-                reply_markup: JSON.stringify({
-                  inline_keyboard: [[{ text: "От кого?", url: `https://vk.com/id${data.object.from_id}`}, 
-                  { text: "Подробнее", url: `https://vk.com/market-124949590?w=product-124949590_${data.object.item_id}`}]]})}
             await bot.sendMessage(rudi, 'Платёж через VK Pay:')
-            await bot.sendMessage(rudi, `${data.object.amount} руб. с комментарием: ${data.object.description}`, options3)
+            await bot.sendMessage(rudi, `${data.object.amount} руб. с комментарием: ${data.object.description}`, inlineKeyboard
+                (`https://vk.com/id${data.object.from_id}`, `https://vk.com/market-124949590?w=product-124949590_${data.object.item_id}`))
                   bot.sendMessage(rudi, line) 
             break
     }
@@ -85,7 +76,6 @@ try {
 module.exports = getData
 
 bot.startPolling(); 
-
 
 
 
