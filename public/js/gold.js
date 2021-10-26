342a4b0c86108c8e53101e6524efa1629fe936ea
Yuri <yur0n@yahoo.com>
2021-10-26 03:04:05 +0300

    GOLDENBOT with web store demo implementation

---
+++ public/js/gold.js
@@ -0,0 +1,31 @@
+const nameForm = document.querySelector('form')
+const input = document.querySelector('#inputId')
+const message = document.querySelector('#message0')
+const message1 = document.querySelector('#message1')
+
+function isCheck() {
+    let goodId = document.querySelector('input[name="buy"]:checked').id
+    if (goodId == 'buy1') return 360
+    if (goodId == 'buy2') return 191
+    if (goodId == 'buy3') return 254
+    if (goodId == 'buy4') return 362 
+}
+
+
+nameForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let bonus = isCheck()
+    fetch(`/goldSilent?bonus=${bonus}&name=${input.value}`).then((response) => {
+    response.json().then((data = {}) => {
+        if (data.error) message.textContent = data.error
+        else { 
+            message.textContent = data.message
+            message1.textContent = "Для просмотра баланса бонусов, отправьте GOLDENBOT'у своё имя"
+            setTimeout( ()=>{
+                message.textContent = ` `
+                message1.textContent = ` `
+            }, 8000)
+        }
+    })
+})
+})
